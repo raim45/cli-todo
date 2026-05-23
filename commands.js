358c4646021524279d0aef8ff647411de1367ff6
Ephraim Oshawo <zackgaminguniverse@gmail.com>
2026-05-23 13:32:30 +0100

    integrated process.argv for terminal inputs, added some edge cases and responsive output on command

--- commands.js
+++ commands.js
@@ -4,11 +4,15 @@ const add = (task) => {
   const tasks = read();
   const length = tasks.length == 0 ? 1: tasks[tasks.length -1].id + 1
   tasks.push({ id: length, task: task, done: false });
-  write(tasks);
+  write(tasks)
+  console.log("Task successfully added")
 };
 
 const list = () => {
   const tasks = read();
+  if (tasks.length == 0){
+    return console.log("No task yet")
+  }
   tasks.forEach((task) => {
     console.log(`${task.id}. ${task.task} ${task.done ? "✓" : "✗"}`);
   });
@@ -23,16 +27,24 @@ const done = (idt) => {
 
   item.done = true;
   write(tasks);
+  return console.log("Task sucessfully checked")
 };
 
 const delT = (idt) => {
   const tasks = read();
+  const item = tasks.find((check) => check.id == Number(idt));
+  if (!item) {
+    return console.log("id does not exist");
+  }
+
   const remaining = tasks.filter((task) => task.id != Number(idt));
-  write(remaining);
+  write(remaining)
+  return console.log("successfully deleted")
 };
 
 const clear = () => {
   write([]);
+  return console.log("All tasks have been cleared")
 };
 
 module.exports = { add, list, done, delT, clear };
--- todo.js
+++ todo.js
@@ -1,1 +1,13 @@
 const { add, list, done, delT, clear } = require("./commands.js");
+
+const [command, val] = process.argv.slice(2)
+
+const commands = {add, list, done,  del: delT, clear }
+const func = commands[command]
+
+if (func){
+    func(val)
+} else{
+    console.log("This command does not exist")
+}
+
